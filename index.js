5103074fa692f75cd88496306223aae8018e2db2
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-01 19:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerLeft();
+  }
+});
 
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
-});
+}
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowRight") {
-    let rightNumbers = dodger.style.right.replace("px", "");
-    let right = parseInt(rightNumbers, 10);
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
-    dodger.style.right = `${right + 1}px`;
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
-});
+}
