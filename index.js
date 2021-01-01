db3cbfd90aa645b39c46cc066e5d9bf3f2fe5b5c
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-01 19:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ document.addEventListener("keydown", function(e) {
     dodger.style.left = `${left - 1}px`;
   }
 });
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+
+    dodger.style.right = `${right + 1}px`;
+  }
+});
